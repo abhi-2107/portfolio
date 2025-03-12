2d8d07cea1653e3f7cc86006fe160e37dfbf2e36
Abhishek panchal <abhipan.ap33@gmail.com>
2025-03-12 18:19:44 +0530

    add skills component

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import "./App.css";
 import Herosection from "./components/Herosection";
 import Navbar from "./components/Navbar";
+import Skills from "./components/Skills";
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
       <div className="bg-gradient-to-br dark:from-stone-950 dark:to-zinc-900 dark:text-slate-50 from-orange-50 to-cyan-50 text-slate-700 min-h-screen font-normal px-3  sm:px-5 container mx-auto">
         <Navbar />
         <Herosection />
+        <Skills />
       </div>
     </>
   );
